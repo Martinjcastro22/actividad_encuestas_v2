d16bec0cb9d26ab55d6cb4faba0e0e73d6de0b93
Martincastro22 <tinchocab22@gmail.com>
2023-10-12 09:59:51 -0300

    Subiendo encuestas

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => (
+    <div>
+        <h2>Pagina no encontrada</h2>
+        <p>Lo sentimos, la pagina que esta buscando no existe</p>
+    </div>
+);
+
+export default NotFound;
+
